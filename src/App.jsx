ab98604ddc3bf4e69bfeb36741af529708e9ad97
Sergey Kostyukevich <76240257+citizenofgreatsiberia@users.noreply.github.com>
2024-08-17 11:12:53 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,9 @@ export default function App() {
 
   return (
     <>
-      <Header/>
-      <ProgressBar />
+      <Header>
+        <ProgressBar />
+      </Header>
       <Outlet />
       <BackToTop />
     </>
